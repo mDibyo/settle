551dbaac1cc11161971f910fb32d926d06d91974
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2014-07-17 12:00:18 -0700

    Added person class

--- src/graph/Person.java
+++ src/graph/Person.java
@@ -2,5 +2,18 @@ package graph;
 
 public class Person {
 
+	protected String firstName;
+	protected String lastName;
+	public String userId;
+	private String password;
+	
+	public Person(String userId, String password) {
+		this.userId = userId;
+		this.password = password;
+	}
+	
+	boolean isUser(String password) {
+		return this.password.equals(password);
+	}
 	
 }
