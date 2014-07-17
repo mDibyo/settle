e36e95f6e73b6dfb67f600495d47a48e435e3831
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2014-07-17 12:12:56 -0700

    Updated Person class

--- src/graph/Person.java
+++ src/graph/Person.java
@@ -4,16 +4,67 @@ public class Person {
 
 	protected String firstName;
 	protected String lastName;
-	public String userId;
+	public final String userId;
 	private String password;
+	public String email;
 	
-	public Person(String userId, String password) {
+	/**
+	 * Create a new person in the system
+	 * @param userId The userId of the person
+	 * @param password The password of the person
+	 * @param email The email address of the person
+	 */
+	public Person(String userId, String password, String email) {
 		this.userId = userId;
 		this.password = password;
+		this.email = email;
 	}
 	
+	/**
+	 * Authenticate the user
+	 * @param password The password entered by the user
+	 * @return whether the authentication succeeded
+	 */
 	boolean isUser(String password) {
 		return this.password.equals(password);
+	}
+	
+	/**
+	 * Set first name of person
+	 * @param firstName The first name of the person
+	 */
+	void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	/**
+	 * Set last name of person
+	 * @param lastName The last name of the person
+	 */
+	void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	/**
+	 * Set email of person
+	 * @param email The email of person
+	 */
+	void setEmail(String email) {
+		this.email = email;
+	}
+	
+	/**
+	 * Set password of person
+	 * @param oldPassword The old password of person
+	 * @param newPassword The new password of person
+	 * @return Whether the operation succeeded
+	 */
+	boolean setPassword(String oldPassword, String newPassword) {
+		if (this.password.equals(oldPassword)) {
+			this.password = newPassword;
+			return true;
+		}
+		return false;
 	}
 	
 }
